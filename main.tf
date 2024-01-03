84df737507ad874874c33127742d1f92afb91f31
karlagabriel <karla.gabriel@hashicorp.com>
2024-01-03 17:10:29 -0600

    feat: Create new role

---
+++ main.tf
@@ -0,0 +1,16 @@
+module "test_s3_bucket" {
+
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.15.1"
+
+  bucket = var.bucket_name
+  versioning = {
+    enabled = var.enable_versioning
+  }
+}
+
+module "iampolicy" {
+  source  = "tfe.karla-gabriel.sbx.hashidemos.io/kg-dev/iampolicy/aws"
+  version = "1.0.1"
+  role_name = "kg-test-role"
+}
